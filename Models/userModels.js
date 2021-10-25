a7de0a158167e9f3a534a3eb4cd4c691c792dfec
SHAHNAD <nshahnad90@gmail.com>
2021-10-25 19:57:13 +0530

    changes

--- Models/userModels.js
+++ Models/userModels.js
@@ -9,7 +9,7 @@ module.exports = class userModel {
 
     loginValidate(data) {
         const { email, password } = data
-        const query = `SELECT _id, email, gender, created_at, updated_at FROM users WHERE email = ? AND password = ?`
+        const query = `SELECT * FROM users WHERE email = ? AND password = ?`
         return db.execute(query, [email, password])
     }
     fetchAll() {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,20 @@
 const userModel = require('../Models/userModels');
 const UserModel = require('../Models/userModels')
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 exports.login = (req, res, next) => {
     const { email, password } = req.body
+    // bcrypt.compare(password, users.password).then((res)=>{
+
+    // }).catch((err)=>{
+        
+    // })
     const user = new userModel({ email, password })
     user.loginValidate({ email, password }).then(([rows, fieldData]) => {
         if (rows?.length) {
             const token = jwt.sign({ email }, 'my_secret_key');
-            res.status(200).send({ data: rows, message: 'Logged In Successfully', status: 200, token });
+            res.status(200).send({ data: rows[0], message: 'Logged In Successfully', status: 200, token });
         } else {
             res.status(404).send('Not Found')
         }
@@ -16,7 +22,4 @@ exports.login = (req, res, next) => {
         res.status(500).send('Internal Server Error')
 
     })
-
-
-
 }
